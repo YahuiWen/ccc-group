fdd9f700a04a6fd3c52b56e1513a9025c3b4b7e5
YahuiWen <wenyahui1997@gmail.com>
2021-04-21 17:28:09 +0800

    koa init

---
+++ app.js
@@ -0,0 +1,14 @@
+const Koa = require('koa')
+const app = new Koa()
+// Middleware
+//first layer
+const  durMidd = require('./middleware/koa_response_duration')
+app.use(durMidd)
+//second layer
+const  headerMidd = require('./middleware/koa_response_header')
+app.use(headerMidd)
+//third layer
+const  dataMidd = require('./middleware/koa_response_data')
+app.use(dataMidd)
+
+app.listen(7777)
