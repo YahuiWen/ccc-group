31f74b920d57968586b3fd40959461dcc31e5a24
xinghantong <1356263735@qq.com>
2021-05-10 12:03:15 +0800

    use one database

--- stream/DB_Communicator.py
+++ stream/DB_Communicator.py
@@ -1,5 +1,5 @@
 import couchdb
-
+from shapely.geometry import Polygon, box
 from crawlerSetting import host, port, username, password, db_name
 
 
@@ -22,6 +22,18 @@ database_select = connect_to_database("test_db", server)
 ######################################################
 
 
+bbox_shapes = {}
+for name, coordinates in bbox.items():
+    bbox_shapes[name] = box(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
+
+
+def preprocess(bounding_box):
+    user_shape = Polygon(bounding_box)
+    for name_, area_shape in bbox_shapes.items():
+        if area_shape.contains(user_shape.centroid):
+            return name_
+    return "Australia other"
+
 
 # data: the dictionary format of tweet object
 def send_to_db_raw(tweet_, db=database_raw):
@@ -29,6 +41,10 @@ def send_to_db_raw(tweet_, db=database_raw):
     # set tweet id as the document id for duplication removal
     tweet_["_id"] = "%d" % tweet_["id"]
     tweet_["id"] = "%d" % tweet_["id"]
+
+    tweet_["geo_code"] = preprocess(tweet_['place']["bounding_box"]["coordinates"][0])  # ADD
+    print(tweet_["geo_code"])
+
     if tweet_ is not None:
         try:
             db.save(tweet_)
@@ -40,6 +56,10 @@ def send_to_db_raw(tweet_, db=database_raw):
 def send_to_db_city(tweet_, db):
 
     tweet_["_id"] = "%s" % tweet_["id"]
+
+    tweet_["geo_code"] = preprocess(tweet_['place']["bounding_box"]["coordinates"][0])  # ADD
+    print(tweet_["geo_code"])
+
     if tweet_ is not None:
         try:
             db.save(tweet_)
--- stream/tweetStreamer.py
+++ stream/tweetStreamer.py
@@ -1,19 +1,18 @@
 
 import tweepy
 from tweepy.streaming import StreamListener
-# from api_requirements import DOMAIN, API_KEY, API_PORT
 from tweepy import OAuthHandler
 from tweepy import Stream
 import argparse
 from datetime import datetime
-# from tweetProcess import uploadImg, postRequest
-# from DB_Communicator import send_to_db_raw, send_to_db_city
 import time
 import json
-import os
-from multiprocessing import Process
 import couchdb
 from crawlerSetting import host, port, username, password, db_name
+from shapely.geometry import Polygon, box
+from uuid import uuid4
+
+
 
 def connect_to_couch_db_server(host, port, username, password):
     secure_remote_server = couchdb.Server('http://' + username + ':' + password + '@' + host + ':' + port)
@@ -29,36 +28,132 @@ def connect_to_database(database_name, server):
 ## SETUP ######################################################
 server = connect_to_couch_db_server(host, port, username, password)
 database_raw = connect_to_database("raw_tweets", server)
-# database_select = connect_to_database("test_db", server)
+database_sydney = connect_to_database("db_sydney", server)
+database_melbourne = connect_to_database("db_melbourne", server)
+database_brisbane = connect_to_database("db_brisbane", server)
+database_adelaide = connect_to_database("db_adelaide", server)
+database_perth = connect_to_database("db_perth", server)
+database_hobart = connect_to_database("db_hobart", server)
+database_canberra = connect_to_database("db_canberra", server)
+database_darwin = connect_to_database("db_darwin", server)
 ######################################################
 
+# 8 capital cities
+bbox = {
+        "sydney": [150.163056, -34.589722, 153.679723, -29],
+        "melbourne": [141, -39.188889, 147.038611, -36.561667],
+        "brisbane": [141, -29, 153.358056, -10.508056],
+        "adelaide": [129, -38.070833, 141, -25.997778],
+        "perth": [112.841389, -35.254166, 129, -13.504166],
+        "hobart": [143.766111, -43.691389, 148.691389, -39.188889],
+        "canberra": [147.038611, -36.561667, 150.163056, -34.589722],
+        "darwin": [129, -25.997778, 141, -10.833333]
+    }
 
+# boxes = ["great_syd", "great_mel", "great_brisbane", "great_ald"]
+
+
+bbox_shapes = {}
+for name, coordinates in bbox.items():
+    bbox_shapes[name] = box(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
+
+
+def preprocess(bounding_box):
+    user_shape = Polygon(bounding_box)
+    for name_, area_shape in bbox_shapes.items():
+        if area_shape.contains(user_shape.centroid):
+            return name_
+    return "Australia other"
+
+
+# data: the dictionary format of tweet object
+def send_to_db_city(tweet_, city_name):
+    tweet_copy = tweet_.copy()
+
+    if city_name == "melbourne":
+        database_melbourne.save(tweet_copy)
+        return
+    elif city_name == "sydney":
+        database_sydney.save(tweet_copy)
+        return
+    elif city_name == "brisbane":
+        database_brisbane.save(tweet_copy)
+        return
+    elif city_name == "perth":
+        database_perth.save(tweet_copy)
+        return
+    elif city_name == "adelaide":
+        database_adelaide.save(tweet_copy)
+        return
+    elif city_name == "canberra":
+        database_canberra.save(tweet_copy)
+        return
+    elif city_name == "hobart":
+        database_hobart.save(tweet_copy)
+        return
+    elif city_name == "darwin":
+        database_darwin.save(tweet_copy)
+        return
+    print("done")
 
 # # data: the dictionary format of tweet object
 def send_to_db_raw(tweet_, db=database_raw):
     
     # set tweet id as the document id for duplication removal
-    tweet_["_id"] = "%d" % tweet_["id"]
-    tweet_["id"] = "%d" % tweet_["id"]
+    tweet_["_id"] = "%s" % tweet_["id"]
+    tweet_["id"] = "%s" % tweet_["id"]
+    # tweet_["_id"] = str(tweet_["id"])
+    # doc = {'_id': uuid4().hex, 'type': 'person', 'name': 'John Doe'}
+
+    # tweet_["_id"] = uuid4().hex
+    tweet_["geo_code"] = preprocess(tweet_['place']["bounding_box"]["coordinates"][0])  # ADD
+    print(tweet_["geo_code"])
+    print(tweet_["_id"])
+
     if tweet_ is not None:
+
         try:
             db.save(tweet_)
+            # send_to_db_city(tweet_, tweet_["geo_code"])
         except:
             print("error")
             pass
 
-# data: the dictionary format of tweet object
-def send_to_db_city(tweet_, db):
+        # db.save(tweet_)
 
-    # set tweet id as the document id for duplication removal
-    tweet_["_id"] = "%d" % tweet_["id"]
-    tweet_["id"] = "%d" % tweet_["id"]
-    if tweet_ is not None:
-        try:
-            db.save(tweet_)
-        except:
-            print("error")
-            pass
+        # send_to_db_city(tweet_, str(tweet_["geo_code"]))
+
+        # city_name = str(tweet_["geo_code"])
+        # print(city_name)
+        # new_tweet_ = tweet_.copy()
+        # if city_name == "melbourne":
+        #     database_melbourne.save(new_tweet_)
+        # elif city_name == "sydney":
+        #     database_sydney.save(new_tweet_)
+        # elif city_name == "brisbane":
+        #     database_brisbane.save(new_tweet_)
+        # elif city_name == "perth":
+        #     database_perth.save(new_tweet_)
+        # elif city_name == "adelaide":
+        #     database_adelaide.save(new_tweet_)
+        # elif city_name == "canberra":
+        #     database_canberra.save(new_tweet_)
+        # elif city_name == "hobart":
+        #     database_hobart.save(new_tweet_)
+        # elif city_name == "darwin":
+        #     database_darwin.save(new_tweet_)
+        # else:
+        #     pass
+        
+        # print(tweet_)
+        # tweet_["_id"] = uuid4().hex
+
+        # tweet_["_id"] = uuid4().hex
+
+        # send_to_db_city(tweet_, str(tweet_["geo_code"]))
+
+
+
 
 
 # My keys and tokens
@@ -72,19 +167,7 @@ access = {"consumer_key": consumer_key,
             "access_token": access_token,
             "access_secret": access_token_secret}
 
-# 8 capital cities
-cors = {
-        "sydney": [150.163056, -34.589722, 153.679723, -29],
-        "melbourne": [141, -39.188889, 147.038611, -36.561667],
-        "brisbane": [141, -29, 153.358056, -10.508056],
-        "adelaide": [129, -38.070833, 141, -25.997778],
-        "perth": [112.841389, -35.254166, 129, -13.504166],
-        "hobart": [143.766111, -43.691389, 148.691389, -39.188889],
-        "canberra": [147.038611, -36.561667, 150.163056, -34.589722],
-        "darwin": [129, -25.997778, 141, -10.833333]
-    }
 
-# boxes = ["great_syd", "great_mel", "great_brisbane", "great_ald"]
 
 # Get the authentication
 def getAuth(access):
@@ -141,7 +224,6 @@ def fieldSelect(tweetJson):
 
 
 def dealStream(tweetJson, file):
-    pass
 
     try:
         send_to_db_raw(tweetJson)
@@ -163,8 +245,10 @@ def dealStream(tweetJson, file):
 parser = argparse.ArgumentParser(description='COMP90024 Project Twitter Streamer')
 # Use like:
 # python arg.py -l 1234 2345 3456 4567
-# parser.add_argument('-l','--list', nargs='+', default=[141, -38, 150, -34])
-parser.add_argument('--filename', type=str, default="streamlog57.txt")
+
+
+parser.add_argument('-l','--list', nargs='+', default=[112.841389, -43.691389, 153.679723, -10.508056])
+parser.add_argument('--filename', type=str, default="streamlog58.txt")
 args = parser.parse_args()
 
 
@@ -177,7 +261,7 @@ class TweetListener(StreamListener):
     def on_data(self, data):
 
         tweetJson = json.loads(data, encoding= 'utf-8')
-        print(tweetJson)
+        # print(tweetJson)
     	# need to filter out the retweets
         if not tweetJson["text"].startswith('RT') and tweetJson["retweeted"] == False:
             file.write(data)
@@ -206,22 +290,18 @@ def stream_city(city, city_cor):
 
 if __name__ == '__main__':
 
-
-
-
-
-    # cfs = load_configs()
-    jobs = []
     
-    for key, value in cors.items():
-        print(key)
-        print(value)
-        
-        p = Process(target=stream_city, args=((key, value)), daemon=True)
-        jobs.append(p)
-        p.start()
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    listener = TweetListener()
+    auth = getAuth(access)
+    stream = Stream(auth, listener)
 
-    [p.join() for p in jobs]
+    #This line filter Twitter Streams to capture data around Victoria state
+    stream.filter(locations=args.list) 
+
+    file.close()
+
+
 
 
 
